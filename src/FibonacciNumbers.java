57102a12a0e3f4b7603c6f536231022232d106c7
Max <sem4enko_max@ukr.net>
2021-02-01 01:41:23 +0200

    updated homework

--- src/FibonacciNumbers.java
+++ src/FibonacciNumbers.java
@@ -1,23 +1,24 @@
-import java.util.Scanner;
+import java.util.concurrent.RecursiveTask;
 
-public class FibonacciNumbers {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please input number:");
-        int n = in.nextInt();
-        System.out.println("Fibonacci number is " + fib(n));
+public class FibonacciNumbers extends RecursiveTask<Long> {
+    private long fib;
+
+    public FibonacciNumbers(long fib) {
+        this.fib = fib;
     }
 
-    public static long fib(int n) {
-        long arr[] = new long[n + 1];
-
-        arr[0] = 0;
-        arr[1] = 1;
-
-        for (int i = 2; i <= n; i++)
-            arr[i] = arr[i - 1] + arr[i - 2];
-        return arr[n];
+    @Override
+    protected Long compute() {
+        if (fib <= 0) {
+            return fib;
+        } else {
+            FibonacciNumbers f1 = new FibonacciNumbers(fib - 1);
+            f1.fork();
+            FibonacciNumbers f2 = new FibonacciNumbers(fib - 2);
+            return f1.join() + f2.compute();
+        }
     }
 }
+
 
 
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+    ForkJoinPool fj = ForkJoinPool.commonPool();
+    FibonacciNumbers fibonacciNumbers = new FibonacciNumbers(50);
+
+    Long output = -(fj.invoke(fibonacciNumbers));
+    System.out.println(output);dv
+}
